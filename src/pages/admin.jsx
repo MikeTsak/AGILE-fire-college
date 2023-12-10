3300b353b04cd40361073389cd7be497be73d9ce
Mike <32255755+MikeTsak@users.noreply.github.com>
2023-12-10 19:10:54 +0200

    Small changes

--- src/pages/admin.jsx
+++ src/pages/admin.jsx
@@ -1,6 +1,7 @@
+// admin.jsx
 import { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
-import axios from 'axios'; // CORS policy
+import axios from 'axios';
 import styles from '../styles/Admin.module.css';
 import NewsDrawer from './components/NewsDrawer';
 import CourcesDrawer from './components/CourcesDrawer';
